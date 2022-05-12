028e4efaac6d03a9969d3fc67780147ac56eca99
John-Lucy-Arup <95297135+John-Lucy-Arup@users.noreply.github.com>
2022-05-11 17:21:43 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,127 +1,216 @@
 // import "./style.css";
 
-import * as THREE from "https://unpkg.com/three@0.127.0/build/three.module.js";
-import { OrbitControls } from "https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js";
-const scene = new THREE.Scene();
+// import * as THREE from "https://unpkg.com/three@0.127.0/build/three.module.js";
+// import { OrbitControls } from "https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js";
+// const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
+// const camera = new THREE.PerspectiveCamera(
+//   75,
+//   window.innerWidth / window.innerHeight,
+//   0.1,
+//   1000
+// );
 
-const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector("#bg"),
-});
+// const renderer = new THREE.WebGLRenderer({
+//   canvas: document.querySelector("#bg"),
+// });
 
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
-camera.position.setX(-3);
+// renderer.setPixelRatio(window.devicePixelRatio);
+// renderer.setSize(window.innerWidth, window.innerHeight);
+// camera.position.setZ(30);
+// camera.position.setX(-3);
 
-renderer.render(scene, camera);
-// object
-// Torus
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
-const torus = new THREE.Mesh(geometry, material);
-//adding to the scene below
+// renderer.render(scene, camera);
+// // object
+// // Torus
+// const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+// const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+// const torus = new THREE.Mesh(geometry, material);
+// //adding to the scene below
 
-scene.add(torus);
+// scene.add(torus);
 
-// Lights
-const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(5, 5, 5);
+// // Lights
+// const pointLight = new THREE.PointLight(0xffffff);
+// pointLight.position.set(5, 5, 5);
 
-const ambientLight = new THREE.AmbientLight(0xffffff);
-scene.add(pointLight, ambientLight);
+// const ambientLight = new THREE.AmbientLight(0xffffff);
+// scene.add(pointLight, ambientLight);
 
-// const lightHelper = new THREE.PointLightHelper(pointLight)
-// const gridHelper = new THREE.GridHelper( 200, 50 )
-// scene.add(lightHelper, gridHelper)
+// // const lightHelper = new THREE.PointLightHelper(pointLight)
+// // const gridHelper = new THREE.GridHelper( 200, 50 )
+// // scene.add(lightHelper, gridHelper)
 
-// const controls = new OrbitControls(camera, renderer.domElement); // Listen to dom events on the mouse and update the camera pos accordingly
+// // const controls = new OrbitControls(camera, renderer.domElement); // Listen to dom events on the mouse and update the camera pos accordingly
 
-function addStar() {
-  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
-  const star = new THREE.Mesh(geometry, material);
+// function addStar() {
+//   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+//   const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+//   const star = new THREE.Mesh(geometry, material);
 
-  const [x, y, z] = Array(3)
-    .fill()
-    .map(() => THREE.MathUtils.randFloatSpread(100));
+//   const [x, y, z] = Array(3)
+//     .fill()
+//     .map(() => THREE.MathUtils.randFloatSpread(100));
 
-  star.position.set(x, y, z);
-  scene.add(star);
-}
-// Arra of 250 values and then for each value calls the addStar function
-Array(250).fill().forEach(addStar);
+//   star.position.set(x, y, z);
+//   scene.add(star);
+// }
+// // Arra of 250 values and then for each value calls the addStar function
+// Array(250).fill().forEach(addStar);
 
-// changes bg
-const spaceTexture = new THREE.TextureLoader().load('space.jpg')
-scene.background = spaceTexture;
+// // changes bg
+// const spaceTexture = new THREE.TextureLoader().load('space.jpg')
+// scene.background = spaceTexture;
 
-// avatar
+// // avatar
 
-const aniTexture = new THREE.TextureLoader().load('ani.png')
-const ani = new THREE.Mesh(
-  new THREE.BoxGeometry(6, 6, 6),
-  //map prop on the material as a texture
-  new THREE.MeshBasicMaterial({ map: aniTexture })
-);
-scene.add(ani);
+// const aniTexture = new THREE.TextureLoader().load('ani.png')
+// const ani = new THREE.Mesh(
+//   new THREE.BoxGeometry(6, 6, 6),
+//   //map prop on the material as a texture
+//   new THREE.MeshBasicMaterial({ map: aniTexture })
+// );
+// scene.add(ani);
 
-//MOOON
-const moonTexture = new THREE.TextureLoader().load('moon.jpg')
-const imageTexture = new THREE.TextureLoader().load('normal.jpg')
-const moon = new THREE.Mesh(
-  new THREE.SphereGeometry(3, 32, 32),
-  new THREE.MeshStandardMaterial({ map: moonTexture, normalMap: imageTexture })
-);
-scene.add(moon);
-// Repostionisng moon to further down of z axis as that is the direction of scroll
-moon.position.z = 30;
-moon.position.setX(-10);
+// //MOOON
+// const moonTexture = new THREE.TextureLoader().load('moon.jpg')
+// const imageTexture = new THREE.TextureLoader().load('normal.jpg')
+// const moon = new THREE.Mesh(
+//   new THREE.SphereGeometry(3, 32, 32),
+//   new THREE.MeshStandardMaterial({ map: moonTexture, normalMap: imageTexture })
+// );
+// scene.add(moon);
+// // Repostionisng moon to further down of z axis as that is the direction of scroll
+// moon.position.z = 30;
+// moon.position.setX(-10);
 
-ani.position.z = -5;
-ani.position.x = 2;
+// ani.position.z = -5;
+// ani.position.x = 2;
 
-// scroll Animation
+// // scroll Animation
 
-function moveCamera() {
-  // top prop here shows how far we are from the top of the webpage
-  const t = document.body.getBoundingClientRect().top;
-  moon.rotation.x += 0.05;
-  moon.rotation.y += 0.075;
-  moon.rotation.z += 0.05;
+// function moveCamera() {
+//   // top prop here shows how far we are from the top of the webpage
+//   const t = document.body.getBoundingClientRect().top;
+//   moon.rotation.x += 0.05;
+//   moon.rotation.y += 0.075;
+//   moon.rotation.z += 0.05;
 
-  ani.rotation.y += 0.01;
-  ani.rotation.z += 0.01;
+//   ani.rotation.y += 0.01;
+//   ani.rotation.z += 0.01;
 
-  camera.rotation.z = t * -0.01;
-  camera.rotation.x = t * -0.0002;
-  camera.rotation.y = t * -0.0002;
-}
-// fire the func when scrolled
-document.body.onscroll = moveCamera;
-moveCamera(); // thr func is assigned as the event handler for the document body on scroll event
+//   camera.rotation.z = t * -0.01;
+//   camera.rotation.x = t * -0.0002;
+//   camera.rotation.y = t * -0.0002;
+// }
+// // fire the func when scrolled
+// document.body.onscroll = moveCamera;
+// moveCamera(); // thr func is assigned as the event handler for the document body on scroll event
 
-// Animation
+// // Animation
 
-// renderer.render( scene, camera );
-// Alternate below
-function animate() {
-  requestAnimationFrame(animate);
+// // renderer.render( scene, camera );
+// // Alternate below
+// function animate() {
+//   requestAnimationFrame(animate);
 
-  torus.rotation.x += 0.01; // rotation along x axis
-  torus.rotation.y += 0.005; // roatation along y axis
-  torus.rotation.z += 0.01; // rotation on z axis
+//   torus.rotation.x += 0.01; // rotation along x axis
+//   torus.rotation.y += 0.005; // roatation along y axis
+//   torus.rotation.z += 0.01; // rotation on z axis
 
-  moon.rotation.x += 0.005;
-  //controls.update();
+//   moon.rotation.x += 0.005;
+//   //controls.update();
 
-  renderer.render(scene, camera);
-}
+//   renderer.render(scene, camera);
+// }
 
-animate();
+// animate();
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="https://use.typekit.net/vcu1fze.css">
+
+
+
+    <meta charset="utf-8">
+    <title>My first three.js app</title>
+    <style>
+        body {
+            margin: 0;
+            color: #ffffff;
+        }
+
+        #info {
+            font-family: gineso-condensed, sans-serif;
+            font-weight: 800;
+            font-style: normal;
+            font-size: 2rem;
+            position: absolute;
+            top: 10px;
+            width: 100%;
+            text-align: center;
+            z-index: 100;
+            display: block;
+            font-size:50
+        }
+    </style>
+</head>
+<body>
+
+    <div id="info">Birmingham Commonwealth Games test copy
+
+
+    <script async src="https://unpkg.com/es-module-shims@1.3.6/dist/es-module-shims.js"></script>
+
+    <script type="importmap">
+        {
+            "imports": {
+                "three": "https://unpkg.com/three/build/three.module.js"
+            }
+        }
+    </script>
+    <script type="module">
+
+        import * as THREE from 'three';
+
+        // init
+
+        const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
+        camera.position.z = 1;
+
+        const scene = new THREE.Scene();
+
+        const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+        const material = new THREE.MeshNormalMaterial();
+
+        const mesh = new THREE.Mesh(geometry, material);
+        scene.add(mesh);
+
+        const renderer = new THREE.WebGLRenderer({ antialias: true });
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.setAnimationLoop(animation);
+        document.body.appendChild(renderer.domElement);
+
+        // animation
+
+        function animation(time) {
+
+            mesh.rotation.x = time / 2000;
+            mesh.rotation.y = time / 1000;
+
+            renderer.render(scene, camera);
+
+        }
+    </script>
+
+
+
+
+
+    <!--<script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>-->
+    <!-- Use it like any other HTML element -->
+    <!--<model-viewer alt="Neil Armstrong's Spacesuit from the Smithsonian Digitization Programs Office and National Air and Space Museum" src="https://github.com/google/model-viewer/blob/master/packages/shared-assets/models/Astronaut-Unlit.glb" ar ar-modes="webxr scene-viewer quick-look" environment-image="shared-assets/environments/moon_1k.hdr" poster="shared-assets/models/NeilArmstrong.webp" seamless-poster shadow-intensity="1" camera-controls enable-pan></model-viewer>-->
+</body>
+</html>
